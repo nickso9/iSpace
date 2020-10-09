2ace07154a4113a50169a246aeeced69a5b9c42b
nicksandoval <sand8val@gmail.com>
2020-10-08 20:03:04 -0700

    wall starter

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -5,9 +5,6 @@ const { ensureAuthenticated } = require('../config/auth');
 
 router.get('/dashboard', ensureAuthenticated, (req, res) => {
     
-    // db.users.findAll({ where: { id: req.user.id}, include: 'posts'}).then(u => console.log(u))
-
-
     db.users.findAll({ 
         attributes: ['id', 'email', 'regDone'],
         where: { id: req.user.id }, 
@@ -20,21 +17,24 @@ router.get('/dashboard', ensureAuthenticated, (req, res) => {
                 attributes: ['headline', 'text', 'createdAt']
             }
         ],
-        // include: 'profile', include: 'posts', 
+        
+
         }).then(userdata => {
+
              
         if (userdata[0].dataValues.regDone == 0) {
             res.redirect('../registration')
         } else {
+            let { id, email, regDone, profile, posts } = userdata[0].dataValues
             const user = {
-                id: userdata[0].dataValues.id,
-                email: userdata[0].dataValues.email,
-                regDone: userdata[0].dataValues.regDone,
-                profile: userdata[0].dataValues.profile.dataValues,
-                posts: userdata[0].dataValues.posts
+                id,
+                email,
+                regDone,
+                posts,
+                profile: profile.dataValues,
             }
-           
-            
+            user.posts.forEach(e => console.log(e.dataValues))
+
             res.render('dashboard', { layout: 'main', user })
         }
 
