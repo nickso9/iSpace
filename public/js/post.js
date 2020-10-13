2b796cd0029b58cbeea4085cf147f813e27b3488
nicksandoval <sand8val@gmail.com>
2020-10-12 21:57:17 -0700

    progress

--- public/js/post.js
+++ public/js/post.js
@@ -10,7 +10,6 @@ $('.post-form').on('submit', (e) => {
     const id = e.target.id
     if (!newHeadline || !newText) {
         $('#small-error-update').show()
-        console.log('in here')
     } else {
     
     const newPosts = {
@@ -56,4 +55,45 @@ $('.wall-delete-btn').on('click', (e) => {
          .catch(err => console.log(err))
     
      
+})
+
+
+// wall post // 
+
+$('.post-form-wall').on('submit', (e) => {
+    e.preventDefault()
+   
+    const newHeadlineWall = $('.headline-text-wall').val()
+    const newTextWall = $('.post-text-wall').val()
+    // const id = e.target.id
+    console.log(newHeadlineWall)
+    console.log(newTextWall)
+
+    if (!newHeadlineWall || !newTextWall) {
+        $('#small-error-update-wall').show()
+        console.log('in here')
+    } else {
+        $('#small-error-update-wall').hide()
+        $('#small-success-update-wall').show()
+    // const newPosts = {
+    // headline: newHeadline,
+    // text: newText,
+    // userId: id,
+    // postId: id
+
+    }    
+
+    // $.ajax(`/users/${e.target.id}/posts`, {
+    //     method: 'PUT',
+    //     data: newPosts
+    //     })
+    //     .then(() => {
+    //         setTimeout(function(){
+    //             window.location.reload();
+    //      }, 200);
+    //     })
+    //     .catch(err => console.log(err))
+
+    // }
+
 })
