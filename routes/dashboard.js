2be9285458166098c8d8778a702c2a88b747bb99
nicksandoval <sand8val@gmail.com>
2020-10-12 20:46:38 -0700

    post working

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -61,91 +61,90 @@ router.get('/dashboard', ensureAuthenticated, (req, res) => {
                 })
                 arrFeed.sort((a, b) =>  b.id - a.id)
                 
-            function findData(userdata) { 
-                return userdata[0].dataValues.friends.map(j => { 
-                const userFriend = j.friendlist
-                return Profile.findOne({ where: { userId: userFriend}, attributes: ['userId','image', 'username', 'location', 'bio', 'headline', 'birthday']})
-                    .then(user => ( user.dataValues ))  
-                    .catch(err => console.log(err))  
-
-                })        
-           }
-    
-           Promise.all(findData(userdata)) 
-            .then(userFriend => {  
-                function findDataPending(userdata) {        
-                    return userdata[0].dataValues.pendingfriends.map(j => {    
-                    return Profile.findOne({ where: { userId: j.newFriendId}, attributes: ['userId','image', 'username', 'location']})
-                        .then(userR => userR.dataValues)
-                        .catch(err => console.log(err))                     
-                    })     
-                }
-
-                Promise.all(findDataPending(userdata)) 
-                .then(userPendFriend => {
-
-                    let { id, email, regDone, profile } = userdata[0].dataValues
-
-                    function findPostData(userdata) {
-                        return db.posts.findAll({ where: { postId: userdata[0].dataValues.id }, attributes: ['headline', 'text', 'createdAt', 'userId', 'id', 'postId']}).then(async postData => {
-                             const promiseMap = await Promise.all(postData.map(async postSmallData => { 
-                                    const promiseMapReturn = await db.profiles.findAll({ where: { userId: postSmallData.dataValues.postId }, attributes: ['image', 'username']}).then(e => {
-                                        return {
-                                        headline: postSmallData.dataValues.headline,
-                                        text: postSmallData.dataValues.text,
-                                        createdAt: postSmallData.dataValues.createdAt,
-                                        id: postSmallData.dataValues.id,
-                                        image: e[0].dataValues.image,
-                                        username: e[0].dataValues.username
-                                        }
-                                    })
-                                    .catch(err => console.log(err))   
-                                    return promiseMapReturn
-                            }))
-                            return promiseMap
-                        })
+                function findData(userdata) { 
+                    return userdata[0].dataValues.friends.map(j => { 
+                    const userFriend = j.friendlist
+                    return Profile.findOne({ where: { userId: userFriend}, attributes: ['userId','image', 'username', 'location', 'bio', 'headline', 'birthday']})
+                        .then(user => ( user.dataValues ))  
                         .catch(err => console.log(err))  
-                    }
-                    Promise.all([findPostData(userdata)])
-                    .then(e => {
-                        console.log(e)
-                        
-                    })
-                    .catch(err => console.log(err))
 
-
-
-
-                    let arr = []
-                        userdata[0].dataValues.posts.forEach(e => {
-                        e.dataValues['image'] = profile.dataValues.image
-                        e.dataValues['username'] = profile.dataValues.username
-                        arr.unshift(e.dataValues)   
-                        })
-
-                        const user = {
-                            id,
-                            email,
-                            regDone,
-                            posts: arr,
-                            feed: arrFeed,
-                            profile: profile.dataValues,
-                            pendingFriends: userPendFriend,
-                            friends: userFriend
-                        }
-                            res.render('dashboard', { layout: 'main', user })
-        
-                });
-                
+                    })        
+                }
             
-                
-            }) 
+                Promise.all(findData(userdata)) 
+                    .then(userFriend => {  
 
-        }) 
+                        function findDataPending(userdata) {        
+                            return userdata[0].dataValues.pendingfriends.map(j => {    
+                            return Profile.findOne({ where: { userId: j.newFriendId}, attributes: ['userId','image', 'username', 'location']})
+                                .then(userR => userR.dataValues)
+                                .catch(err => console.log(err))                     
+                            })     
+                        }
+
+                        Promise.all(findDataPending(userdata)) 
+                        .then(userPendFriend => {
+
+                        
+                        let { id, email, regDone, profile } = userdata[0].dataValues
+
+                            function findPostData(userdata) {
+                                return db.posts.findAll({ where: { postId: userdata[0].dataValues.id }, attributes: ['headline', 'text', 'createdAt', 'userId', 'id', 'postId']}).then(async postData => {
+                                    const promiseMap = await Promise.all(postData.map(async postSmallData => { 
+                                            const promiseMapReturn = await db.profiles.findAll({ where: { userId: postSmallData.dataValues.postId }, attributes: ['image', 'username']}).then(e => {
+                                                return {
+                                                headline: postSmallData.dataValues.headline,
+                                                text: postSmallData.dataValues.text,
+                                                createdAt: postSmallData.dataValues.createdAt,
+                                                id: postSmallData.dataValues.id,
+                                                image: e[0].dataValues.image,
+                                                username: e[0].dataValues.username
+                                                }
+                                            })
+                                            .catch(err => console.log(err))   
+                                            return promiseMapReturn
+                                    }))
+                                    return promiseMap
+                                })
+                                .catch(err => console.log(err))  
+                            }
+
+                            Promise.all([findPostData(userdata)])
+                            .then(userWallData => {
+                                let wallData = userWallData[0]
+                                
+                                wallData.sort((a, b) =>  b.id - a.id)
+
+                             
+
+
+                                // let arr = []
+                                //     userdata[0].dataValues.posts.forEach(e => {
+                                //     e.dataValues['image'] = profile.dataValues.image
+                                //     e.dataValues['username'] = profile.dataValues.username
+                                //     arr.unshift(e.dataValues)   
+                                //     })
+
+                                    const user = {
+                                        id,
+                                        email,
+                                        regDone,
+                                        posts: wallData,
+                                        feed: arrFeed,
+                                        profile: profile.dataValues,
+                                        pendingFriends: userPendFriend,
+                                        friends: userFriend
+                                    }
+                                        res.render('dashboard', { layout: 'main', user })
+                            })
+                            .catch(err => console.log(err))
+                        });   
+                }) 
+
+            }) 
         }
     })
     .catch(err => console.log(err))
-
 })
 
 module.exports = router
